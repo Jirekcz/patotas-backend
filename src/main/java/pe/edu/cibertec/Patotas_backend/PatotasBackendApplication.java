92ff774447bdf37947d0baac74ba1eab25162f38
Jireh Flores <Jirekcz03ft@gmail.com>
2024-09-21 15:38:41 -0500

    Carga Inicial del proyecto BackEnd Patotas

---
+++ src/main/java/pe/edu/cibertec/Patotas_backend/PatotasBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Patotas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatotasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatotasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Patotas_backend/PatotasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Patotas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatotasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
