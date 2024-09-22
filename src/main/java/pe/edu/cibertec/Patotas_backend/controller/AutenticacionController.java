de35d8f7a7cf1a2db5eedd1ec89107c2ab0c25e8
Jireh Flores <Jirekcz03ft@gmail.com>
2024-09-22 15:19:22 -0500

    Fix Annotation AutenticacionController

--- src/main/java/pe/edu/cibertec/Patotas_backend/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/Patotas_backend/controller/AutenticacionController.java
@@ -1,13 +1,13 @@
 package pe.edu.cibertec.Patotas_backend.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pe.edu.cibertec.Patotas_backend.dto.LoginRequestDTO;
 import pe.edu.cibertec.Patotas_backend.dto.LoginResponseDTO;
 
-@Controller
+@RestController
 @RequestMapping("/autenticacion")
 public class AutenticacionController {
 
